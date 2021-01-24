b12158f3dd56db74a29ce7c5ec1dba1ecd8d8d89
= <itzsymbaa@outlook.com>
2021-01-24 22:06:30 +0000

    Added basic pie chart for score split, not resetting properly yet

--- app/src/main/java/com/spudg/sentient/VisualiserActivity.kt
+++ app/src/main/java/com/spudg/sentient/VisualiserActivity.kt
@@ -8,7 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.spudg.sentient.databinding.ActivityVisualiserBinding
 import com.spudg.sentient.databinding.DayMonthYearPickerBinding
 import com.spudg.sentient.databinding.MonthYearPickerBinding
@@ -17,8 +25,14 @@ import kotlin.collections.ArrayList
 
 class VisualiserActivity : AppCompatActivity() {
 
+    private var entriesPie: ArrayList<PieEntry> = ArrayList()
+    private var entriesBar: ArrayList<BarEntry> = ArrayList()
+
     private lateinit var bindingVisualiser: ActivityVisualiserBinding
     private lateinit var bindingMonthYearPicker: MonthYearPickerBinding
+
+    var scoreSplit: ArrayList<Int> = arrayListOf()
+    var scoreTitles: ArrayList<String> = arrayListOf()
 
     var monthFilter = Calendar.getInstance()[Calendar.MONTH] + 1
     var yearFilter = Calendar.getInstance()[Calendar.YEAR]
@@ -57,12 +71,15 @@ class VisualiserActivity : AppCompatActivity() {
             }
 
             bindingMonthYearPicker.submitMy.setOnClickListener {
-                //makePieData(monthFilter, yearFilter)
-                //makeBarData(monthFilter, yearFilter, 1)
+
+                makeBarData(monthFilter, yearFilter)
+                makePieData(monthFilter, yearFilter)
+
+                setUpBarChart()
+                setUpPieChart()
+
                 setMonthHeader(monthFilter, yearFilter)
-                //setupPieChartIncome()
-                //setupPieChartExpenditure()
-                //setupBarChart()
+
                 setUpScoreNumberText()
                 setUpNoteList()
                 filterDialog.dismiss()
@@ -82,10 +99,129 @@ class VisualiserActivity : AppCompatActivity() {
             finish()
         }
 
-        setUpScoreNumberText()
-        setUpNoteList()
         setMonthHeader(monthFilter, yearFilter)
 
+        setUpScoreNumberText()
+
+        makeBarData(monthFilter,yearFilter)
+        setUpBarChart()
+
+        makePieData(monthFilter,yearFilter)
+        setUpPieChart()
+
+        setUpNoteList()
+
+
+    }
+
+    private fun resetBarData() {
+
+    }
+
+    private fun makeBarData(monthFilter: Int, yearFilter: Int) {
+
+    }
+
+    private fun setUpBarChart() {
+
+    }
+
+    private fun resetPieData() {
+        scoreSplit = arrayListOf()
+        scoreTitles = arrayListOf()
+    }
+
+    private fun makePieData(monthFilter: Int, yearFilter: Int) {
+
+        resetPieData()
+
+        val db = RecordHandler(this, null)
+
+        val records = db.getRecordsForMonthYear(monthFilter, yearFilter)
+
+        var score0to9 = 0
+        var score10to39 = 0
+        var score40to69 = 0
+        var score70to89 = 0
+        var score90to100 = 0
+
+        for (record in records) {
+            when (record.score) {
+                in 0..9 -> {
+                    score0to9 += 1
+                }
+                in 10..39 -> {
+                    score10to39 += 1
+                }
+                in 40..69 -> {
+                    score40to69 += 1
+                }
+                in 70..89 -> {
+                    score70to89 += 1
+                }
+                in 90..100 -> {
+                    score90to100 += 1
+                }
+
+            }
+        }
+
+        scoreSplit = arrayListOf(score0to9,score10to39,score40to69,score70to89,score90to100)
+        scoreTitles = arrayListOf("0 to 9","10 to 39", "40 to 69", "70 to 89", "90 to 100")
+
+        db.close()
+
+    }
+
+    private fun setUpPieChart() {
+
+        if (scoreSplit.size > 0) {
+            for (i in 0 until scoreSplit.size) {
+                entriesPie.add(PieEntry(scoreSplit[i].toFloat(), scoreTitles[i]))
+            }
+
+            val dataSetPie = PieDataSet(entriesPie, "")
+            //dataSetExp.colors = categoryColoursExp.toMutableList()
+            val dataPie = PieData(dataSetPie)
+
+            val chartPie: PieChart = bindingVisualiser.chartSplitAveScore
+            if (entriesPie.size > 0) {
+                chartPie.data = dataPie
+            }
+
+            //chartExp.animateY(800)
+            //chartExp.setNoDataText("No net expenditure categories for the month selected.")
+            //chartExp.setNoDataTextColor(0xff000000.toInt())
+            //chartExp.setNoDataTextTypeface(ResourcesCompat.getFont(this, R.font.open_sans_light))
+            //chartExp.setEntryLabelTypeface(ResourcesCompat.getFont(this, R.font.open_sans_light))
+            //chartExp.dragDecelerationFrictionCoef = .95f
+            //chartExp.setDrawEntryLabels(false)
+
+            //chartExp.description.isEnabled = false
+
+            //val l: Legend = chartExp.legend
+            //l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+            //l.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+            //l.orientation = Legend.LegendOrientation.HORIZONTAL
+            //l.setDrawInside(false)
+
+            //dataSetExp.valueLinePart1OffsetPercentage = 80f
+            //dataSetExp.valueLinePart1Length = 0.4f
+            //dataSetExp.valueLinePart2Length = 0.8f
+            //dataSetExp.yValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
+
+            //dataExp.setValueFormatter(PercentFormatter())
+            //dataExp.setValueTextSize(11f)
+            //dataExp.setValueTextColor(Color.BLACK)
+
+            chartPie.invalidate()
+
+        } else {
+            bindingVisualiser.chartSplitAveScore.clear()
+            bindingVisualiser.chartSplitAveScore.setNoDataText("No net expenditure categories for the month selected.")
+            bindingVisualiser.chartSplitAveScore.setNoDataTextColor(0xff000000.toInt())
+            bindingVisualiser.chartSplitAveScore.setNoDataTextTypeface(ResourcesCompat.getFont(this, R.font.open_sans_light))
+        }
     }
 
     private fun setUpNoteList() {
@@ -100,20 +236,6 @@ class VisualiserActivity : AppCompatActivity() {
             bindingVisualiser.rvNotes.visibility = View.GONE
             bindingVisualiser.tvNoNotes.visibility = View.VISIBLE
         }
-    }
-
-    private fun getRecordList(): ArrayList<RecordModel> {
-        val dbHandler = RecordHandler(this, null)
-        val result = dbHandler.filterRecords(-1)
-        dbHandler.close()
-        return result
-    }
-
-    private fun getRecordListMonthYear(month: Int, year: Int): ArrayList<RecordModel> {
-        val dbHandler = RecordHandler(this, null)
-        val result = dbHandler.getRecordsForMonthYear(month, year)
-        dbHandler.close()
-        return result
     }
 
     private fun getRecordListMonthYearWithNoteOnly(month: Int, year: Int): ArrayList<RecordModel> {
